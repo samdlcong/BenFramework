3b570c7695a580bb28019d2a4180430f66ea82ec
samnew <samnew@126.com>
2016-08-20 23:34:02 +0800

    add auto load class

--- core/ben.php
+++ core/ben.php
@@ -2,11 +2,26 @@
 namespace core;
 
 class ben{
+	static public $classMap=array();
+
 	static public function run(){
-		p('ok');
+		//p('ok');
+		$route = new \core\lib\route();
+		
 	}
 	static public function load($class){
 		//自动加载类库
-		
+		if(isset(self::$classMap[$class])){
+			return true;
+		}else{
+			$class = str_replace('\\', '/', $class);
+			$file = BEN.'/'.$class.'.php';
+			if(is_file($file)){
+				include $file;
+				self::$classMap[$class] = $class;
+			}else{
+				return false;
+			}
+		}
 	}
 }
