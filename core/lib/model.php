7fab9b961fa9364d279073a19550122ccd5c9d5e
samnew <samnew@126.com>
2016-08-21 22:01:21 +0800

    add modle class file;

---
+++ core/lib/model.php
@@ -0,0 +1,15 @@
+<?php
+namespace core\lib;
+
+class model extends \PDO {
+	public function __construct(){
+		$dsn = 'mysql:host=localhost;dbname=test';
+		$username ='root';
+		$passwd = '123456';
+		try{
+			parent::__construct($dsn,$username,$passwd);
+		}catch (\PDOException $e){
+			p($e->getMessage());
+		}
+	}
+}
