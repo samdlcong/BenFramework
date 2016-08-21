35366e09a38848bb70ac62e39a6e1e6190760bd6
samnew <samnew@126.com>
2016-08-21 22:02:33 +0800

    modify modle class test

--- app/ctrl/indexCtrl.php
+++ app/ctrl/indexCtrl.php
@@ -3,5 +3,10 @@ namespace app\ctrl;
 class indexCtrl {
 	public function index(){
 		p('it is index');
+		$model = new \core\lib\model();
+		$sql = 'select * from user';
+		$res = $model->query($sql);
+		$arr = $res->fetchAll(\PDO::FETCH_ASSOC);
+		p($arr);
 	}
 }
